b7ef950296cfd9b26d703cc4b4aaf5444357f26d
aparat123 <dima.holubenko@gmail.com>
2016-03-07 13:46:32 +0200

    app from Andriy

---
+++ myVk/app/src/main/java/ua/jcode/vk/VideoFragment.java
@@ -0,0 +1,26 @@
+package ua.jcode.vk;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class VideoFragment extends Fragment {
+
+    public VideoFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.video_fragment, container, false);
+    }
+
+}
